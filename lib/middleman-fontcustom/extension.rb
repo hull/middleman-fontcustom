3730b33ac7c6f3b0b91c89dc1653bd295651d173
Romain Dardour <romain@hull.io>
2015-02-01 11:44:11 +0100

    Add missing options

--- lib/middleman-fontcustom/extension.rb
+++ lib/middleman-fontcustom/extension.rb
@@ -6,6 +6,14 @@ module Middleman
     option :source_dir, 'assets/fontcustom', 'Folder contains icon files'
     option :fonts_dir, 'source/fonts', 'Folder to output fonts'
     option :css_dir, 'source/stylesheets', 'Folder to output css'
+
+    option :css_prefix, 'glyphicon-{{glyph}}', 'Glyph Prefix'
+    option :autowidth, true, 'Autowidth Glyphs'
+    option :font_design_size, 32, 'Original Glyph Size'
+    option :font_em, 1800, 'Scaling Font'
+    option :font_ascent, 1600, 'Font Ascent'
+    option :font_descent, 200, 'Font Descent'
+
     option :templates, 'scss', 'Output templates'
     option :no_hash, true, 'Create hash for no cache policy'
     option :preprocessor_path, nil, 'Relative path from your compiled CSS to your output directory'
@@ -25,6 +33,12 @@ module Middleman
             :fonts => config[:fonts_dir],
             :css => config[:css_dir]
           },
+          :css_prefix => config[:css_prefix],
+          :autowidth => config[:autowidth],
+          :font_design_size => config[:font_design_size],
+          :font_em => config[:font_em],
+          :font_ascent => config[:font_ascent],
+          :font_descent => config[:font_descent],
           :templates => config[:templates].split(/\s/),
           :no_hash => config[:no_hash],
           :preprocessor_path => config[:preprocessor_path]
